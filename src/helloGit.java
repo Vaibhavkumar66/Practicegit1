970d7876d97d71c3b3eb0daf39d317326202e941
vaibhav <bvaibhav6@gmail.com>
2021-12-10 15:52:06 -0500

    just v1

---
+++ src/helloGit.java
@@ -0,0 +1,10 @@
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("practice git again" );
+         int a=5;
+         int b=6;
+         int c= ((a+b)/2);
+        System.out.println(c);
+
+    }
+}
